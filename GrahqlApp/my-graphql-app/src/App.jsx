442c6f5b108e2b02e18392d1bcd0280f313fd0bc
AvayKpatra <147364657+AvayKpatra@users.noreply.github.com>
2024-02-22 11:28:26 +0530

    Graphql application

---
+++ GrahqlApp/my-graphql-app/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import UserData from "./components/UserData";
+import 
+
+function App() {
+  return (
+    <>
+      <h1>Graphql app</h1>
+      <UserData />
+    </>
+  );
+}
+
+export default App;
